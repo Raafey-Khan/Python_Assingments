22ac5a9cdfd15b4775170a2617b756d9588c6087
RaafayKhan123 <113880768+RaafayKhan123@users.noreply.github.com>
2022-09-20 01:59:24 +0530

    Add files via upload

---
+++ Python assingment (1)/A3.py
@@ -0,0 +1,3 @@
+print("MySirG")
+
+print("Teacher's Day")
---
+++ Python assingment (1)/A4.py
@@ -0,0 +1,9 @@
+from tkinter import N
+
+
+name = "Raafay Khan"
+age = 18
+Qualification = "BCA first year"
+years_of_experience = 2
+
+print(name , age , Qualification , years_of_experience)
